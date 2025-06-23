2c107e39bcf9befed91c165df93f7b47613576a1
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-23 16:02:54 +0100

    Seed user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+first_user = User.find_or_initialize_by(email: "peter@mail.com")
+first_user.assign_attributes(password: "peterpeter")
+first_user.save
+
+second_user = User.find_or_initialize_by(email: "olivia@mail.com")
+second_user.assign_attributes(password: "oliviaolivia")
+second_user.save
