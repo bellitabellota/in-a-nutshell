ba1e5c5b7c8cd4514bcc245b48e2206ccb54fc6b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-24 16:28:12 +0100

    Make current_user.profile data available on client side via Context API

--- app/javascript/bundles/components/App.jsx
+++ app/javascript/bundles/components/App.jsx
@@ -1,12 +1,16 @@
 
 import routes from "../routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CurrentUserProfileContext from "./CurrentUserProfileContext";
 
 const router = createBrowserRouter(routes);
 
-function App() {
+
+function App({currentUserProfile}) {
   return (
-    <RouterProvider router={router} />
+    <CurrentUserProfileContext.Provider value={currentUserProfile}>
+      <RouterProvider router={router} />
+    </CurrentUserProfileContext.Provider>
   )
 }
 
---
+++ app/javascript/bundles/components/CurrentUserProfileContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const CurrentUserProfileContext = createContext(null);
+
+export default CurrentUserProfileContext;
