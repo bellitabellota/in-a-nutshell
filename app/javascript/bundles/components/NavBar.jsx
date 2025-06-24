82ec49c6cb84df6f49e8d6224dad99bd32ce2fb7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-24 16:28:32 +0100

    Create NavBar component

---
+++ app/javascript/bundles/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import CurrentUserProfileContext from "./CurrentUserProfileContext";
+
+function NavBar() {
+  const currentUserProfile = useContext(CurrentUserProfileContext);
+  return(
+    <div>
+      <Link to="/">Chats</Link>
+      <Link to="/contacts">Contacts</Link>
+      <Link to={`/profile/${currentUserProfile.connectToken}`}>My Profile</Link>
+    </div>
+  )
+}
+
+export default NavBar;
