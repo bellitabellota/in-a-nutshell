1a78cbc389f57b63b1178e195c16a3bd5b271f39
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-25 13:32:12 +0100

    Add ':create_inverse_if_not_exists' after_create to ensure mutual relationship

--- app/models/user_contact.rb
+++ app/models/user_contact.rb
@@ -1,4 +1,16 @@
 class UserContact < ApplicationRecord
   belongs_to :user
   belongs_to :contact, class_name: "User"
+
+  after_create :create_inverse_if_not_exists
+
+  private
+
+  def inverse_exists?
+    UserContact.exists?(user: self.contact, contact: self.user)
+  end
+
+  def create_inverse_if_not_exists
+    UserContact.create(user: self.contact, contact: self.user) unless inverse_exists?
+  end
 end
