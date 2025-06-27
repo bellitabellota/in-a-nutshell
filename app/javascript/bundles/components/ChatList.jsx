a7dbd4d255aab39299adab65e92eeff46c4718f8
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-27 15:26:52 +0100

    Render contacts in ChatList component

--- app/javascript/bundles/components/ChatList.jsx
+++ app/javascript/bundles/components/ChatList.jsx
@@ -1,10 +1,18 @@
 import NavBar from "./NavBar";
+import { useContext } from "react";
+import CurrentUserContext from "../CurrentUserContext";
 
 function ChatList() {
+  const currentUser = useContext(CurrentUserContext);
+
+  const contacts = currentUser.contacts.map((contact) => {
+    return <div className="contact-container" key={contact.id}>{contact.profile.name}</div>
+  })
+
   return(
   <>
     <h1>Chats component</h1>
-    
+    {contacts}
     <NavBar />
   </>
     
