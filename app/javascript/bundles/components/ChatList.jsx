4a7d0ab5b1d45ca096a7d839d6d5b44654674f90
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-27 15:48:36 +0100

    Make each contact on the Chats page a link to the associated chat

--- app/javascript/bundles/components/ChatList.jsx
+++ app/javascript/bundles/components/ChatList.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./NavBar";
 import { useContext } from "react";
 import CurrentUserContext from "../CurrentUserContext";
+import { Link } from "react-router-dom";
 
 function ChatList() {
   const currentUser = useContext(CurrentUserContext);
 
   const contacts = currentUser.contacts.map((contact) => {
-    return <div className="contact-container" key={contact.id}>{contact.profile.name}</div>
+    return <Link to={`/chats/${contact.chatId}`} key={contact.id}><div className="contact-container" >{contact.profile.name}</div></Link>
+    
   })
 
   return(
--- app/javascript/bundles/routes.jsx
+++ app/javascript/bundles/routes.jsx
@@ -8,7 +8,7 @@ const routes = [{
   path: "/",
   element: <ChatList />
 }, {
-  path: "/:chatId",
+  path: "/chats/:chatId",
   element: <Chat />
 },
 {
