3f0b25bad8f5b1f6dba1777201edc8ec39fb934c
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-27 15:24:49 +0100

    Expand props sent to App component with the user's contacts (profile inclusive)

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,26 @@
 class ChatsController < ApplicationController
   def index
+    contact_ids = current_user.contacts.pluck(:id)
+    contacts = User.includes(:profile).where(id: contact_ids)
+
+    contact_profiles = contacts.map do |contact|
+      {
+        id: contact.id,
+        profile: {
+          name: contact.profile.name,
+          info: contact.profile.info,
+          connectToken: contact.profile.connect_token
+        }
+      }
+    end
+
+    @react_props = {
+      profile: {
+        name: current_user.profile.name,
+        info: current_user.profile.info,
+        connectToken: current_user.profile.connect_token
+      },
+      contacts: contact_profiles
+    }
   end
 end
