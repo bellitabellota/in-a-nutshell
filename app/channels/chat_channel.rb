089d42a63c7ca95bd85db965237b6d13d03996f5
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-29 12:09:37 +0100

    Establish stream

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,7 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    chat = Chat.find(params[:id])
+    stream_for chat
   end
 
   def unsubscribed
