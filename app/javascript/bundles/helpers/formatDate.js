ebd25eecbd5ce41ab9b81cb208a5c3a80f311792
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-07 14:27:40 +0100

    Add formatDate helper

---
+++ app/javascript/bundles/helpers/formatDate.js
@@ -0,0 +1,22 @@
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const now = new Date();
+
+  // the below get methods will convert the saved UTC time into the local time
+  const isToday = date.getFullYear() === now.getFullYear() &&
+                  date.getMonth() === now.getMonth() &&
+                  date.getDate() === now.getDate();
+
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  if (isToday) {
+    return `${hours}:${minutes}`;
+  } else {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    return `${day}. ${month} ${hours}:${minutes}`;
+  }
+}
+
+export default formatDate;
