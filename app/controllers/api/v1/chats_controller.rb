cbf9678d257f17c4b3dac70b521349f8577c5387
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-09 12:10:38 +0100

    Order messages for their creation date

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ChatsController < ApplicationController
   def show
     chat = Chat.find(params[:id])
 
-    messages = chat.messages.map do |message|
+    messages = chat.messages.order(:created_at).map do |message|
       { id: message.id,
         author: message.author_id,
         creationDate: message.created_at,
