dfefac721c1c3f3d64b58307bd7e6e243d0fdb73
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-10 11:37:22 +0100

    Create a separate context for contacts

--- app/javascript/bundles/components/App.jsx
+++ app/javascript/bundles/components/App.jsx
@@ -1,7 +1,9 @@
 
 import routes from "../routes";
+import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CurrentUserContext from "./contexts/CurrentUserContext";
+import ContactsContext from "./contexts/ContactsContext";
 import { CableProvider } from "./contexts/cable";
 import horizontalLogo from "../../images/in-a-nutshell-logo-lettering-horizontal-cropped.png";
 
@@ -9,13 +11,16 @@ const router = createBrowserRouter(routes);
 
 
 function App(currentUser) {
+  const [contacts, setContacts] = useState(currentUser.contacts);
   return (
     <div className="app-container">
       <img src={horizontalLogo} className="horizontal-logo"/>
-      <CurrentUserContext.Provider value={currentUser}>
-        <CableProvider>
-          <RouterProvider router={router} />
-        </CableProvider>
+      <CurrentUserContext.Provider value={{ id: currentUser.id, profile: currentUser.profile }}>
+        <ContactsContext.Provider value={{ contacts, setContacts }}>
+          <CableProvider>
+            <RouterProvider router={router} />
+          </CableProvider>
+        </ContactsContext.Provider>
       </CurrentUserContext.Provider>
     </div>
   )
---
+++ app/javascript/bundles/components/contexts/ContactsContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const ContactsContext = createContext(null);
+export default ContactsContext;
