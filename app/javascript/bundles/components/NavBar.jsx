ebc186116da20625c38d6e41a34b7b02911a117b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-10 16:11:11 +0100

    Highlight current navigation item

--- app/javascript/bundles/components/NavBar.jsx
+++ app/javascript/bundles/components/NavBar.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import CurrentUserContext from "./contexts/CurrentUserContext";
 
 function NavBar() {
   const currentUser = useContext(CurrentUserContext);
+  const location = useLocation();
+
+  const isChats = location.pathname === "/" || location.pathname.startsWith("/chats/");
+  const isContacts = location.pathname === "/contacts";
+  const isProfile = location.pathname === `/profile/${currentUser.profile.connectToken}`;
+
   return(
     <nav>
-      <p><Link to="/">Chats</Link></p>
-      <p><Link to="/contacts">Contacts</Link></p>
-      <p><Link to={`/profile/${currentUser.profile.connectToken}`}>My Profile</Link></p>
+      <p className={isChats ? "active-nav" : ""}><Link to="/">Chats</Link></p>
+      <p className={isContacts ? "active-nav" : ""}><Link to="/contacts">Contacts</Link></p>
+      <p className={isProfile ? "active-nav" : ""}><Link to={`/profile/${currentUser.profile.connectToken}`}>My Profile</Link></p>
     </nav>
   )
 }
