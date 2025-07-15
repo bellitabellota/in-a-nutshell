6cb99b67d4ee869cf2e86f76d2a883bae3fc838d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-15 17:42:21 +0100

    Add profiles#update action

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,4 +8,21 @@ class Api::V1::ProfilesController < ApplicationController
       render json: { error: "User not found." }, status: :not_found
     end
   end
+
+  def update
+    profile = Profile.find(params[:id])
+
+    if profile.update(profile_params)
+      render json: profile.as_json.merge({ pictureURL: url_for(profile.picture) })
+    else
+      # if time permits improve error handling
+      render json: { error: "Profile could not be updated." }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :info, :picture)
+  end
 end
