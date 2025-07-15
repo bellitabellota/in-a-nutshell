4e0c082605e0371c2184ba02ec46712d85693d8b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-15 16:48:16 +0100

    Send PATCH request

--- app/javascript/bundles/components/ProfileEditForm.jsx
+++ app/javascript/bundles/components/ProfileEditForm.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useState,  } from "react";
 
 const ProfileEditForm = ({profile}) => {
   const [nameInput, setNameInput] = useState(profile.name);
   const [infoText, setInfoText] = useState(profile.info);
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const updateHandler = () => {
+    const url = `/api/v1/profiles/${profile.id}`;
+    const token = document.querySelector('meta[name="csrf-token"]').content
+    const formData = new FormData();
+
+    formData.append("profile[name]", nameInput);
+    formData.append("profile[info]", infoText);
+
+    if(selectedFile) {
+      formData.append("profile[picture]", selectedFile, selectedFile.name);
+    }
+
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        "X-CSRF-Token": token
+      }, 
+      body: formData
+    })
+  };
+
 
   return(
     <div>
@@ -11,7 +33,7 @@ const ProfileEditForm = ({profile}) => {
       <input type="text" value={nameInput} onChange={(event) => {setNameInput(event.target.value)}}/>
       <p>Connect Token: {profile.connectToken} </p>
       <textarea value={infoText ? infoText : ""} placeholder={"Tell something about yourself..."} onChange={(event) => {setInfoText(event.target.value)}}></textarea>
-      <button>Update</button>
+      <button onClick={updateHandler}>Update</button>
     </div>
   )
 }
