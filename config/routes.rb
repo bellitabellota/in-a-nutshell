572f7331a90757d6476fd9764e3b718988a9a2fd
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-18 17:55:35 +0100

    Add route for user#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users, only: [ :destroy ]
       get "profiles/search"
       resources :profiles, only: [ :update ]
       resources :chats, only: [ :show ] do
