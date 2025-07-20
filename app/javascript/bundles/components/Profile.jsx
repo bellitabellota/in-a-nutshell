1cbc0baa465d7296ed7c62f9cff551f5759de1d4
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-20 08:22:27 +0100

    Lift the decision on which button to render on ProfileCard up to the parent

--- app/javascript/bundles/components/Profile.jsx
+++ app/javascript/bundles/components/Profile.jsx
@@ -4,16 +4,54 @@ import CurrentUserContext from "./contexts/CurrentUserContext";
 import ProfileCard from "./ProfileCard";
 import ProfileEditForm from "./ProfileEditForm";
 import * as styles from "./Profile.module.css"
+import ContactsContext from "./contexts/ContactsContext";
 
 function Profile() {
-  const {currentUser} = useContext(CurrentUserContext);
+  const {currentUser, setCurrentUser} = useContext(CurrentUserContext);
+  const {setContacts} = useContext(ContactsContext)
   const [editingMode, setEditingMode] = useState(false);
+
+  const deleteAccountHandler = () => {
+    if (!window.confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
+      return;
+    }
+
+    const url = "/api/v1/users/destroy"
+    const token = document.querySelector('meta[name="csrf-token"]').content
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token
+      }
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to delete account.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      alert(data.message);
+      setCurrentUser({id: undefined, profile:{connectToken: ""}});
+      setContacts([]);
+      window.location.href = "/users/sign_in";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+  }
   
   return(
     <main className={styles.mainProfile}>
       <NavBar />
       <div className={styles.contentProfile}>
-        { editingMode ? <ProfileEditForm profile={currentUser.profile} setEditingMode={setEditingMode} /> : <ProfileCard profile={currentUser.profile} setEditingMode={setEditingMode}/> }
+        { editingMode ? <ProfileEditForm profile={currentUser.profile} setEditingMode={setEditingMode} /> 
+                      : <ProfileCard profile={currentUser.profile} renderActions={() => (
+                          <>
+                            <button onClick={deleteAccountHandler}>Delete Account</button>
+                            <button onClick={() => setEditingMode(true)}>Edit</button>
+                          </>)}
+                        /> }
       </div>
     </main>  
   )
--- app/javascript/bundles/components/ProfileCard.jsx
+++ app/javascript/bundles/components/ProfileCard.jsx
@@ -4,41 +4,8 @@ import CurrentUserContext from "./contexts/CurrentUserContext";
 import ContactsContext from "./contexts/ContactsContext";
 import { useContext } from "react";
 
-function ProfileCard ({profile, setEditingMode}) {
-  const {setCurrentUser} = useContext(CurrentUserContext)
-  const {setContacts} = useContext(ContactsContext)
-
+function ProfileCard ({profile, renderActions}) {
   const imageSrc = profile.picture || defaultProfilePicture;
-
-  const deleteAccountHandler = () => {
-    if (!window.confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
-      return;
-    }
-
-    const url = "/api/v1/users/destroy"
-    const token = document.querySelector('meta[name="csrf-token"]').content
-
-    fetch(url, {
-      method: "DELETE",
-      headers: {
-        "X-CSRF-Token": token
-      }
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to delete account.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      alert(data.message);
-      setCurrentUser({id: undefined, profile:{connectToken: ""}});
-      setContacts([]);
-      window.location.href = "/users/sign_in";
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-  }
 
   return(
     <div className={styles.profileCard}>
@@ -53,8 +20,7 @@ function ProfileCard ({profile, setEditingMode}) {
           <p className={styles.info}><span className={styles.label}>Info:</span> {profile.info || "--"}</p>
         </div>
         <div className={styles.profileCardActions}>
-          <button onClick={deleteAccountHandler}>Delete Account</button>
-          <button onClick={() => setEditingMode(true)}>Edit</button>
+          {renderActions && renderActions()}
         </div>
       </div>
     </div>
