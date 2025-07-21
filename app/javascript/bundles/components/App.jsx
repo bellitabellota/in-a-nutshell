76162c08aea53ec3689aa786e4bd3e1c079b1029
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-21 10:38:13 +0100

    Add and style logout button

--- app/javascript/bundles/components/App.jsx
+++ app/javascript/bundles/components/App.jsx
@@ -6,6 +6,7 @@ import CurrentUserContext from "./contexts/CurrentUserContext";
 import ContactsContext from "./contexts/ContactsContext";
 import { CableProvider } from "./contexts/cable";
 import horizontalLogo from "../../images/in-a-nutshell-logo-lettering-horizontal-cropped.png";
+import logoutIcon from "../../images/logout-svgrepo-com.svg"
 
 const router = createBrowserRouter(routes);
 
@@ -16,7 +17,10 @@ function App(reactProps) {
   const [contacts, setContacts] = useState(reactProps.contacts);
   return (
     <div className="app-container">
-      <img src={horizontalLogo} className="horizontal-logo"/>
+      <div className="pre-main-container">
+        <img src={horizontalLogo} className="horizontal-logo"/>
+        <button><img src={logoutIcon} className="logout-icon"/></button>
+      </div>
       <CurrentUserContext.Provider value={{ currentUser, setCurrentUser}}>
         <ContactsContext.Provider value={{ contacts, setContacts }}>
           <CableProvider>
