b5611554abf6ce58b4e3f4094a3c91d0d8e3a089
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-21 10:52:43 +0100

    Add logoutHandler

--- app/javascript/bundles/components/App.jsx
+++ app/javascript/bundles/components/App.jsx
@@ -15,11 +15,30 @@ function App(reactProps) {
   const user = {id: reactProps.id, profile:{...reactProps.profile}}
   const [currentUser, setCurrentUser] = useState(user);
   const [contacts, setContacts] = useState(reactProps.contacts);
+
+  const logoutHandler = () => {
+    const token = document.querySelector('meta[name="csrf-token"]').content
+    fetch("/users/sign_out", {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Accept": "application/json",
+      }
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error("Logout failed.")
+      }
+      window.location.href = "/users/sign_in";
+    }).catch((error)=> {
+      alert(error);
+    })
+  }
+
   return (
     <div className="app-container">
       <div className="pre-main-container">
         <img src={horizontalLogo} className="horizontal-logo"/>
-        <button><img src={logoutIcon} className="logout-icon"/></button>
+        <button onClick={logoutHandler}><img src={logoutIcon} className="logout-icon"/></button>
       </div>
       <CurrentUserContext.Provider value={{ currentUser, setCurrentUser}}>
         <ContactsContext.Provider value={{ contacts, setContacts }}>
