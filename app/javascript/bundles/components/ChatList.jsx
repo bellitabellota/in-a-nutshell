70730371a94ee8fb808eeff7bdd30bdb612db173
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-24 12:52:23 +0100

    Make ContactList scroll if exceeding ChatList height

--- app/javascript/bundles/components/ChatList.jsx
+++ app/javascript/bundles/components/ChatList.jsx
@@ -1,9 +1,10 @@
 import NavBar from "./NavBar";
 import ContactList from "./ContactList";
+import * as styles from "./ChatList.module.css"
 
 function ChatList() {
   return(
-  <main>
+  <main className={styles.contactListContent}>
     <NavBar />
     <ContactList />    
   </main>
