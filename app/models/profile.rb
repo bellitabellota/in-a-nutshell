cc1ea0ce832ececeefbeb9aa57dd4e71604dff00
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-27 10:29:53 +0100

    Add presence and uniqueness constraint for name in Profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,6 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :picture, dependent: :destroy
 
+  validates :name, uniqueness: true, presence: true
   validates :connect_token, presence: true
 end
