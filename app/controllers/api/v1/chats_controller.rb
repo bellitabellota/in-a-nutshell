4df32654a87bce3a1d33f1961b94cc3cb13e6ce4
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-07-29 15:50:33 +0100

    Include rich text content and images when querying all messages of a chat

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::ChatsController < ApplicationController
   def show
     chat = Chat.find(params[:id])
 
-    messages = chat.messages.order(:created_at).map do |message|
+    # Avoiding N+1 Queries with Action Text: https://guides.rubyonrails.org/v7.2/action_text_overview.html
+    messages = chat.messages.with_rich_text_content_and_embeds.order(:created_at).map do |message|
       { id: message.id,
         author: message.author_id,
         creationDate: message.created_at,
